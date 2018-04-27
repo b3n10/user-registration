2dc5ed3ede33a23dc6b9d4c32008ce416f8e9433
b3n10 <francisben.lleve@gmail.com>
2018-04-27 14:49:53 +0800

    Separate header file

---
+++ header.php
@@ -0,0 +1,16 @@
+<?php
+	session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title><?php echo $title; ?></title>
+	<link rel="stylesheet" href="style.css">
+	<link href="https://fonts.googleapis.com/css?family=Yantramanav:100,300,400,500,700,900" rel="stylesheet">
+</head>
+<body>
+	<div class="header">
+		<h1><?php echo $heading; ?></h1>
+	</div>
