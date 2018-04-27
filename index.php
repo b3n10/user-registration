d2ba529f6eab8508b8812dc887c89c69d878058f
b3n10 <francisben.lleve@gmail.com>
2018-04-27 13:13:13 +0800

    Init

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title>User Registration</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div class="header">
+		<h1>Register</h1>
+	</div>
+	<div class="user-input">
+		<form action="includes/register.php" method="POST">
+			<input type="text" name="reg_username" placeholder="Username">
+			<input type="text" name="reg_email" placeholder="Email address">
+			<input type="password" name="reg_password" placeholder="Password">
+			<input type="password" name="reg_confirm_password" placeholder="Confirm Password">
+			<button type="submit" name="submit">Submit</button>
+		</form>
+	</div>
+</body>
+</html>
