d9d4598e98f93e33d7a3988928fb12f41b0b3450
b3n10 <francisben.lleve@gmail.com>
2018-04-27 14:54:41 +0800

    Change file to redirect when not logged in

--- index.php
+++ index.php
@@ -1,24 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<title>User Registration</title>
-	<link rel="stylesheet" href="style.css">
-	<link href="https://fonts.googleapis.com/css?family=Yantramanav:100,300,400,500,700,900" rel="stylesheet">
-</head>
-<body>
-	<div class="header">
-		<h1>Register</h1>
-	</div>
-	<div class="user-input">
-		<form action="includes/register.php" method="POST">
-			<input type="text" name="reg_username" placeholder="Username">
-			<input type="text" name="reg_email" placeholder="Email address">
-			<input type="password" name="reg_password" placeholder="Password">
-			<input type="password" name="reg_confirm_password" placeholder="Confirm Password">
-			<button type="submit" name="submit">Submit</button>
-		</form>
-	</div>
-</body>
-</html>
+<?php
+session_start();
+
+if (isset($_SESSION["username"])) {
+	// show login info
+} else {
+	header("Location: ./register.php");
+	exit();
+}
