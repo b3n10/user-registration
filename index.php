a076cffa8e4eafb2c2ff6e06f247d52159f648de
b3n10 <francisben.lleve@gmail.com>
2018-04-27 13:13:57 +0800

    Add link to Yantramanav font

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<title>User Registration</title>
 	<link rel="stylesheet" href="style.css">
+	<link href="https://fonts.googleapis.com/css?family=Yantramanav:100,300,400,500,700,900" rel="stylesheet">
 </head>
 <body>
 	<div class="header">
