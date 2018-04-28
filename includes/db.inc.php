474268678f7dbaf4067b6cf63faef456e45c1fff
b3n10 <francisben.lleve@gmail.com>
2018-04-28 08:15:23 +0800

    Add  to mysqli connection

--- includes/db.inc.php
+++ includes/db.inc.php
@@ -6,7 +6,7 @@ $db_password = "jairah";
 $db_name = "user_registration";
 
 if (isset($_POST["submit"])) {
-	mysqli_connect($db_host, $db_user, $db_password, $db_name);
+	$conn = mysqli_connect($db_host, $db_user, $db_password, $db_name);
 } else {
 	header("Location: ../index.php?error=page not allowed");
 	exit();
