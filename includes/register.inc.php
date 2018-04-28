ee25bd11e8b49b672de5b1c9f6610bfe8fd4f4c5
b3n10 <francisben.lleve@gmail.com>
2018-04-28 13:16:42 +0800

    Add input validation, check user is on record before INSERT SQL

--- includes/register.inc.php
+++ includes/register.inc.php
@@ -2,29 +2,32 @@
 
 require_once "db.inc.php";
 
-$uname = $_POST["reg_username"];
-$emadd = $_POST["reg_email"];
-$pword = $_POST["reg_password"];
-$cpword = $_POST["reg_confirm_password"];
-$error_array = array();
-
-/*
-	if empty input
-		redirect with register error msg = input empty
-	else
-		if user is already registered
-			redirect with register error msg
-		else
-			save to db
-*/
+$uname = htmlspecialchars($_POST["reg_username"]);
+$emadd = htmlspecialchars($_POST["reg_email"]);
+$pword = htmlspecialchars($_POST["reg_password"]);
+$cpword = htmlspecialchars($_POST["reg_confirm_password"]);
 
 // redirect if missing
 if (empty($uname) || empty($emadd) || empty($pword) || empty($cpword)) {
 	header("Location: ../register.php?reg_error=Missing Input(s)!");
-} else {
-	$encrypt_pword = md5($pword);
-	$sql = "INSERT INTO users (username, email_address, password)
-					VALUES ($uname, $emadd, $encrypt_pword);";
-	mysqli_query($conn, $sql);
-	header("Location: ../register.php?reg_success=Record Saved!");
+} else if ($pword != $cpword) {
+	header("Location: ../register.php?reg_error=Passwords mismatch!");
+}	else {
+	// query if username/email is on record
+	$sql = "SELECT * FROM users WHERE username='$uname' OR email_address='$emadd'";
+	$result = mysqli_query($conn, $sql);
+
+	// if username is already on record
+	if (mysqli_num_rows($result) >= 1) {
+		header("Location: ../register.php?reg_error=Username/E-mail Address Already Taken!");
+		exit();
+	// if available
+	} else {
+		$encrypt_pword = md5($pword);
+		$sql = "INSERT INTO users (username, email_address, password)
+			VALUES ('$uname', '$emadd', '$encrypt_pword')";
+		$result = mysqli_query($conn, $sql);
+		$_SESSION["username"] = $uname;
+		header("Location: ../register.php?reg_success=Record Saved!");
+	}
 }
