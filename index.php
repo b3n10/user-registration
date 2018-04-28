9feffb254d32865751c89ee361ffe7271242e184
b3n10 <francisben.lleve@gmail.com>
2018-04-28 13:17:28 +0800

    Update title, heading, add welcome message

--- index.php
+++ index.php
@@ -1,9 +1,23 @@
 <?php
-session_start();
+$title = "Home Page";
+$heading = "Home";
+require_once "header.php";
+?>
 
-if (isset($_SESSION["username"])) {
-	// show login info
-} else {
-	header("Location: ./register.php");
-	exit();
-}
+<?php if (isset($_SESSION["username"])): ?>
+	<div class="welcome-msg">
+		<h1>Welcome, <?php echo $_SESSION["username"]; ?></h1>
+		<p>
+			<a href="includes/logout.inc.php">Logout</a>
+		</p>
+	</div>
+<?php else: ?>
+	<?php
+		header("Location: ./login.php");
+		exit();
+	?>
+<?php endif ?>
+
+<?php
+	require_once "footer.php";
+?>
